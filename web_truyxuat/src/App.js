af6a4c105dadc76a6e111f052461f663015cb0ea
diepdau <92206593+diepdau@users.noreply.github.com>
2023-08-29 03:04:04 +0700

    navbar and home
    hoanthanh
    Su dung boostrap

--- web_truyxuat/src/App.js
+++ web_truyxuat/src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import Sidebar from './asset/Sidebar/Sidebar';
+import Home from './asset/Home/Home';
 
 function App() {
+    const[toggle, setToggle]=useState(true)
+    const Toggle = () =>{
+      setToggle(!toggle)
+    }
   return (
     <div className='container-fluid bg-secondary min-vh-100'>
       <div className='row'>
-        <div className='col-2 bg-white vh-100'>
+        {toggle && <div className='col-4 col-md-2 bg-white vh-100 position-fixed'>
           <Sidebar/>
+        </div>}
+        {toggle &&<div className='col-4 col-md-2'></div>}
+        <div className='col'>
+          <Home Toggle={Toggle}/>
         </div>
-        <div className='col-auto'></div>
       </div>
       
     </div>
