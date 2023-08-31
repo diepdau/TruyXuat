920f38e7ea75544696d36cdf75a0e702b6d42bc3
DauThiTieuDiep <2115196@dlu.edu.vn>
2023-09-01 03:07:18 +0700

    Login_tam
    don so cong so sai

--- web_truyxuat/src/App.js
+++ web_truyxuat/src/App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import Sidebar from './asset/Sidebar/Sidebar';
+import Login from './asset/Login/Login';
 
 function App() {
   return (
-    <div className='container-fluid bg-secondary min-vh-100'>
-      <div className='row'>
-        <div className='col-2 bg-white vh-100'>
-          <Sidebar/>
-        </div>
-        <div className='col-auto'></div>
-      </div>
+    // <div className='container-fluid bg-secondary min-vh-100'>
+    //   <div className='row'>
+    //     <div className='col-2 bg-white vh-100'>
+    //       <Sidebar/>
+    //     </div>
+    //     <div className='col-auto'></div>
+    //   </div>
       
-    </div>
+    // </div>
+    <Login/>
   );
 }
 
